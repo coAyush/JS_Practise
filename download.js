b827e6526fbb9e40e9a0532968d94ce348db7c09
coAyush <chowdhuryayush7@gmail.com>
2025-12-09 09:41:11 +0530

    Aded setInterval() and other timer functions with dowloading progress as a practise

---
+++ download.js
@@ -0,0 +1,21 @@
+let percent = document.querySelector(".percent");
+let progress = document.querySelector(".progress");
+let btn = document.querySelector(".btn");
+let pr = 0;
+let seconds = 10;
+btn.addEventListener("click", () => {
+    btn.textContent = `Downloading: ${pr}%`;
+    let st = setInterval(() => {
+    if (pr <= 99) {
+        percent.textContent = `${pr}%`
+        progress.style.width = `${pr}%`
+        pr++;
+    }
+    btn.textContent = `Downloading: ${pr}%`;
+    if (pr == 100) {
+        btn.textContent = "Download finished";
+        percent.textContent = `${pr}%`
+
+    }
+}, (seconds * 1000) / 100);
+})
